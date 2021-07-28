a898d677a92608f17941a4df2823d39d235affa0
Olay Gureje <olayg@Olays-MacBook-Air.local>
2021-07-28 11:35:03 -0400

    Initial commit

---
+++ app/src/main/java/com/olayg/taskapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.olayg.taskapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Welcome", Toast.LENGTH_LONG).show()
+    }
+}
